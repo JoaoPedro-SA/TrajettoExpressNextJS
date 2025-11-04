bea8d10ef94fcbac3ff2a99bb1b71499017d16f7
Uatila Santos <uatila.santoss@gmail.com>
2025-11-04 10:37:30 -0300

    Fix: Correção responsividade dashboard

--- src/app/(protected)/dashboard/layout.js
+++ src/app/(protected)/dashboard/layout.js
@@ -1,57 +1,35 @@
-// export default function DashboardLayout({ children }) {
-//   return (
-//     <div style={{ display: "flex" }}>
-//       <aside style={{
-//         width: "300px",
-//         background: "#222",
-//         color: "#fff",
-//         height: "100vh",
-//         padding: "2rem"
-//       }}>
-//         <h3>Painel</h3>
-//         <br></br>
-//         <ul>
-//           {/* <li><a href="/dashboard/perfil">Perfil</a></li> */}
-//           <li><a href="/dashboard/teste">Teste</a></li>
-//           <li><a href="/dashboard/carga">Manifesto de Carga</a></li>
-//           <li><a href="/dashboard/veiculo">Cadastro de Veiculos</a></li>
-//           <li><a href="/dashboard/motorista">Cadastro Motoristas</a></li>
-//           <br></br>
-//           <br></br>
-//           <br></br>
-
-//           <li><a href="/">Logout</a></li>
-//         </ul>
-//       </aside>
-//       <main style={{ padding: "2rem", flex: 1 }}>
-//         {children}
-//       </main>
-//     </div>
-//   );
-// }
-
-
-
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./dashboard.module.css";
 
 export default function DashboardLayout({ children }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className={styles.dashboardContainer}>
-      <aside className={styles.sidebar}>
+      {/*botão menu mobile hambunguer */}
+      <button
+        className={styles.menuToggle}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </button>
+
+      {/*barra menu */}
+      <aside className={`${styles.sidebar} ${menuOpen ? styles.open : ""}`}>
         <div className={styles.logoBox}>
           <Image
             src="/logobranco.png"
             alt="Logo Trajetto Express"
-            width={180}
-            height={80}
+            width={160}
+            height={60}
             priority
             className={styles.logo}
           />
         </div>
 
         <h4>Painel</h4>
-
         <ul>
           <li><a href="/dashboard/carga">Manifesto de Carga</a></li>
           <li><a href="/dashboard/veiculo">Veículos</a></li>
@@ -61,16 +39,14 @@ export default function DashboardLayout({ children }) {
         </ul>
 
         <ul>
-          <li>
-            <a href="/" className={styles.logout}>Logout</a>
-          </li>
+          <li><a href="/" className={styles.logout}>Logout</a></li>
         </ul>
       </aside>
 
+      {/*Conteúdo*/}
       <main className={styles.mainContent}>
         {children}
       </main>
     </div>
   );
 }
-
